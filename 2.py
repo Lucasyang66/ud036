7fba6a90a8def06fe02e1a66c03f8fbf6ddc7bd5
lucas660 <61895803+lucas660@users.noreply.github.com>
2020-03-07 12:11:18 +0800

    ud036
    update to git

---
+++ 2.py
@@ -0,0 +1,13 @@
+import turtle
+brad=turtle.Turtle()
+def draw_aquare():
+    window=turtle.Screen()
+    window.bgcolor("white")
+    brad.shape("turtle")
+    brad.color("black")
+    brad.speed(2)
+    for i in range(4):
+        brad.forward(100)
+        brad.right(90)
+        i+=1
+draw_aquare()
