d2be81d693de022260b3e51d241fb1db5df48a97
lucas660 <61895803+lucas660@users.noreply.github.com>
2020-03-28 11:32:59 +0800

    update
    update

--- checkProfanity.py
+++ checkProfanity.py
@@ -3,16 +3,20 @@ import urllib.request
 def read_text ():
     text=open ("C:\\Users\\lucas\\Desktop\\ud036\\Famous_Movie_Quotes.txt")
     contents_of_file=text.read()
-    text.close()
-    check_curse(contents_of_file)
+    text.close()    
+    print("contents_of_file ",contents_of_file)
+    new_contents_of_file=contents_of_file.split('\n')
+    print('new_contents_of_file ',new_contents_of_file)
+    for x in new_contents_of_file:     
+        check_curse(x)
 def check_curse(text):
-    print(text)
     connection=urllib.request.urlopen("http://www.wdylike.appspot.com/?q="+text)
     output=connection.read()
     connection.close()
-    if  output :
-       print("the curse word is in the document")
+    #print(output)
+    if  'true' in str(output) :
+       print(text," the curse word is in the document")
     else:
-      print("no cuse word")
+      print(text," no cuse word")
 
 read_text()
